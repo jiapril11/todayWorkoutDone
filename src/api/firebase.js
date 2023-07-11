ec242b6c21c81924449801150d7da185e0129935
jiapril11 <tadadadacoding@gmail.com>
2023-07-11 17:16:40 +0900

    Chore, Feat Chore: .yarnrc.yml 파일명 변경 Feat: firebase-signUp, signIn, signOut 셋팅 및 적용

---
+++ src/api/firebase.js
@@ -0,0 +1,59 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
+} from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FB_API_KEY,
+  authDomain: process.env.REACT_APP_FB_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+
+export const signUpWithFB = async (email, password) => {
+  try {
+    const userCredential = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    const user = userCredential.user;
+  } catch (error) {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+  }
+};
+
+export const signInWithFB = async (email, password) => {
+  try {
+    const userCredential = await signInWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    const user = userCredential.user;
+  } catch (error) {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+  }
+};
+export const watchAuthStateChange = (callback) => {
+  onAuthStateChanged(auth, (user) => {
+    callback(user);
+  });
+};
+
+export const signOutWithFB = async () => {
+  await signOut(auth);
+};
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,5 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
+import { signInWithFB } from "../api/firebase";
+import { useNavigate } from "react-router-dom";
 
 export default function SignIn() {
-  return <div>SignIn</div>;
+  const navigate = useNavigate();
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setUser((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const submitUser = async (e) => {
+    e.preventDefault();
+    await signInWithFB(user.email, user.password);
+    // TODO: rtk에 저장
+    // await auth.currentUser;
+    navigate("/");
+  };
+  return (
+    <div>
+      <form onSubmit={submitUser}>
+        <div>
+          <div>
+            <label htmlFor="email">이메일</label>
+            <input
+              id="email"
+              type="email"
+              name="email"
+              value={user.email}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div>
+            <p>warning text</p>
+          </div>
+        </div>
+        <div>
+          <div>
+            <label htmlFor="password">비밀번호</label>
+            <input
+              id="password"
+              type="password"
+              name="password"
+              value={user.password}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div>
+            <p>warning text</p>
+          </div>
+        </div>
+        <div>
+          <button>signIn</button>
+        </div>
+      </form>
+    </div>
+  );
 }
